2926b9bafd4fb138c436de80ff6368a96e0b0ee2
Katie Weinstein <katie.weinstein3@gmail.com>
2021-08-14 22:45:20 -0400

    refactor scoring details into separate component

--- components/Scoring.jsx
+++ components/Scoring.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Pressable } from 'react-native';
 import { styles, buttons, text } from './styles';
 import DailyDoubleModal from './DailyDoubleModal';
+import ScoringDetails from './ScoringDetails';
 
 export default function Scoring({
   scores,
@@ -23,44 +24,15 @@ export default function Scoring({
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
       />
-      <View style={styles.scoreboardButtonContainer}>
-        <Pressable
-          style={[
-            buttons.dailyDouble,
-            typeof moveInfo.player === 'string'
-              ? { backgroundColor: 'grey' }
-              : { backgroundColor: '#DC7C51' },
-          ]}
-          onPress={() => setModalVisible(!modalVisible)}
-          disabled={typeof moveInfo.player === 'string'}
-        >
-          <Text style={text.smallCentered}>Daily Double</Text>
-        </Pressable>
-        <Pressable
-          style={buttons.negativeScore}
-          onPress={() => {
-            setScores(scores.map((item) => item * -1));
-          }}
-        >
-          <Text style={text.smallCentered}>—</Text>
-        </Pressable>
-        <Pressable
-          style={[
-            buttons.doubleJeopardy,
-            double ? { borderColor: 'white' } : { borderColor: '#84495A' },
-          ]}
-          onPress={() => {
-            setDouble(!double);
-            double
-              ? setScores([200, 400, 600, 800, 1000])
-              : setScores([400, 800, 1200, 1600, 2000]);
-          }}
-        >
-          <Text style={text.smallCentered}>
-            {double ? 'Single Jeopardy' : 'Double Jeopardy'}
-          </Text>
-        </Pressable>
-      </View>
+      <ScoringDetails
+        moveInfo={moveInfo}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        scores={scores}
+        setScores={setScores}
+        double={double}
+        setDouble={setDouble}
+      />
       <View style={styles.scoreboardButtonContainer}>
         {scores.length ? (
           scores.map((item, index) => (
---
+++ components/ScoringDetails.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Text, View, Pressable } from 'react-native';
+import { styles, buttons, text } from './styles';
+
+export default function ScoringDetails({
+  moveInfo,
+  modalVisible,
+  setModalVisible,
+  scores,
+  setScores,
+  double,
+  setDouble,
+}) {
+  return (
+    <View style={styles.scoreboardButtonContainer}>
+      <Pressable
+        style={[
+          buttons.dailyDouble,
+          typeof moveInfo.player === 'string'
+            ? { backgroundColor: 'grey' }
+            : { backgroundColor: '#DC7C51' },
+        ]}
+        onPress={() => setModalVisible(!modalVisible)}
+        disabled={typeof moveInfo.player === 'string'}
+      >
+        <Text style={text.smallCentered}>Daily Double</Text>
+      </Pressable>
+      <Pressable
+        style={buttons.negativeScore}
+        onPress={() => {
+          setScores(scores.map((item) => item * -1));
+        }}
+      >
+        <Text style={text.smallCentered}>—</Text>
+      </Pressable>
+      <Pressable
+        style={[
+          buttons.doubleJeopardy,
+          double ? { borderColor: 'white' } : { borderColor: '#84495A' },
+        ]}
+        onPress={() => {
+          setDouble(!double);
+          double
+            ? setScores([200, 400, 600, 800, 1000])
+            : setScores([400, 800, 1200, 1600, 2000]);
+        }}
+      >
+        <Text style={text.smallCentered}>
+          {double ? 'Single Jeopardy' : 'Double Jeopardy'}
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
